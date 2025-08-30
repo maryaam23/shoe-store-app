f68a52c729613682160fb62ce307d75c5a138229
Leena Affouri <leenaaffouri4@gmail.com>
2025-08-30 20:21:20 +0300

    Added product list page

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,4 @@
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.2")
-    }
-}
+
 
 
 allprojects {
--- lib/User_Pages/signup_page.dart
+++ lib/User_Pages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // For SVG icons
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -333,55 +334,103 @@ class _SignupPageState extends State<SignupPage> {
                   const SizedBox(height: 20),
 
                   // Social Buttons
-                  Center(
-                    child: Wrap(
-                      spacing: 12,
-                      runSpacing: 12,
-                      alignment: WrapAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFFe7edf4),
-                            foregroundColor: const Color(0xFF0d141c),
-                            minimumSize: const Size(84, 40),
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                            ),
-                          ),
-                          child: const Text(
-                            'Google',
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFFe7edf4),
-                            foregroundColor: const Color(0xFF0d141c),
-                            minimumSize: const Size(84, 40),
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                            ),
-                          ),
-                          child: const Text(
-                            'Facebook',
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ],
-                    ),
+                  Column(
+                children: [
+                  const Text(
+                    "Or Sign in with",
+                    style: TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w500),
                   ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center, // Center all social icons
+                    children: [
+                      // Google
+                      IconButton(
+                        onPressed: () {
+                          //  handle Google login
+                        },
+                        style: IconButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          side: const BorderSide(color: Colors.grey),
+                        ),
+                        icon: SvgPicture.string(
+                          '''
+                          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 488 512">
+                            <path d="M488 261.8C488 403.3 391.1 504 
+                            248 504 110.8 504 0 393.2 0 256S110.8 
+                            8 248 8c66.8 0 123 24.5 166.3 
+                            64.9l-67.5 64.9C258.5 52.6 94.3 
+                            116.6 94.3 256c0 86.5 69.1 156.6 
+                            153.7 156.6 98.2 0 135-70.4 
+                            140.8-106.9H248v-85.3h236.1c2.3 
+                            12.7 3.9 24.9 3.9 41.4z"/>
+                          </svg>
+                          ''',
+                          width: 24,
+                          height: 24,
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+
+                      // Apple
+                      IconButton(
+                        onPressed: () {
+                          //  handle Apple login
+                        },
+                        style: IconButton.styleFrom(
+                          backgroundColor: Colors.black,
+                        ),
+                        icon: SvgPicture.string(
+                          '''
+                          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 384 512">
+                            <path fill="white" d="M318.7 268.7c-.2-36.7 
+                            16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 
+                            20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 
+                            141.2 4 184.8 4 273.5q0 39.3 14.4 
+                            81.2c12.8 36.7 59 126.7 107.2 
+                            125.2 25.2-.6 43-17.9 75.8-17.9 
+                            31.8 0 48.3 17.9 76.4 17.9 
+                            48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 
+                            24.8-61.9 24-72.5-24.1 1.4-52 
+                            16.4-67.9 34.9-17.5 19.8-27.8 
+                            44.3-25.6 71.9 26.1 2 49.9-11.4 
+                            69.5-34.3z"/>
+                          </svg>
+                          ''',
+                          width: 24,
+                          height: 24,
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+
+                      // Twitter
+                      IconButton(
+                        onPressed: () {
+                          //  handle Twitter login
+                        },
+                        style: IconButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                        ),
+                        icon: SvgPicture.string(
+                          '''
+                          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 512 512">
+                            <path fill="white" d="M389.2 48h70.6L305.6 
+                            224.2 487 464H345L233.7 318.6 106.5 
+                            464H35.8L200.7 275.5 26.8 
+                            48H172.4L272.9 180.9 389.2 
+                            48zM364.4 421.8h39.1L151.1 88h-42L364.4 
+                            421.8z"/>
+                          </svg>
+                          ''',
+                          width: 24,
+                          height: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
 
                   const SizedBox(height: 12),
 
