6c0ef8967015f3bdec26733f8e4b1d5aba8294ad
maryaam23 <1200837@student.birzeit.edu>
2025-09-27 21:05:03 +0300

    Notification

--- lib/User_Pages/user_notification_page.dart
+++ lib/User_Pages/user_notification_page.dart
@@ -1,4 +1,6 @@
+// notification_screen.dart
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -10,73 +12,16 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   int selectedTabIndex = 0;
 
-  // Notifications for each tab
-  final List<Map<String, String>> promotions = const [
-    {
-      'title': 'Summer Sale',
-      'subtitle': 'Limited time offer! Get 20% off on all running shoes.',
-      'image':
-          'https://lh3.googleusercontent.com/aida-public/AB6AXuDiMr-33X4QDduGDebcdfwMOIDXgAI6qowG7XYx-840QI5DR1-NlVU5RrHBI7B9-d5ZGCq_5mkT76qC3_KYrLOikOzRww1cF8f4HFJSbvv4xUj4yBCuPR4n6sI4Iz7UVsbw_ZVkzVbgYOXwC9uKufS8auJfV4ph-c0sQ1AICzlp7h4S8TsihoqAspH0vQ_Bp77Dn-XS7MFUZNvPge6ZSUqQCyI1L0hoPf6H3jJC6yV5nYnyfyg5WyIe-0wSbdO_WG13CoEF2hqmCN0f',
-
-    },
-    {
-      'title': 'Loyalty Program',
-      'subtitle': 'Exclusive discount for our loyal customers.',
-      'image':
-          'https://lh3.googleusercontent.com/aida-public/AB6AXuDiMr-33X4QDduGDebcdfwMOIDXgAI6qowG7XYx-840QI5DR1-NlVU5RrHBI7B9-d5ZGCq_5mkT76qC3_KYrLOikOzRww1cF8f4HFJSbvv4xUj4yBCuPR4n6sI4Iz7UVsbw_ZVkzVbgYOXwC9uKufS8auJfV4ph-c0sQ1AICzlp7h4S8TsihoqAspH0vQ_Bp77Dn-XS7MFUZNvPge6ZSUqQCyI1L0hoPf6H3jJC6yV5nYnyfyg5WyIe-0wSbdO_WG13CoEF2hqmCN0f',
-
-    },
+  final List<String> categories = [
+    "promotions",
+    "order_updates",
+    "recommendations",
   ];
-
-  final List<Map<String, String>> orderUpdates = const [
-    {
-      'title': 'Order #1234',
-      'subtitle': 'Your order has been shipped.',
-      'image':
-          'https://lh3.googleusercontent.com/aida-public/AB6AXuDiMr-33X4QDduGDebcdfwMOIDXgAI6qowG7XYx-840QI5DR1-NlVU5RrHBI7B9-d5ZGCq_5mkT76qC3_KYrLOikOzRww1cF8f4HFJSbvv4xUj4yBCuPR4n6sI4Iz7UVsbw_ZVkzVbgYOXwC9uKufS8auJfV4ph-c0sQ1AICzlp7h4S8TsihoqAspH0vQ_Bp77Dn-XS7MFUZNvPge6ZSUqQCyI1L0hoPf6H3jJC6yV5nYnyfyg5WyIe-0wSbdO_WG13CoEF2hqmCN0f',
-
-    },
-    {
-      'title': 'Order #5678',
-      'subtitle': 'Your order is out for delivery.',
-      'image':
-          'https://lh3.googleusercontent.com/aida-public/AB6AXuDiMr-33X4QDduGDebcdfwMOIDXgAI6qowG7XYx-840QI5DR1-NlVU5RrHBI7B9-d5ZGCq_5mkT76qC3_KYrLOikOzRww1cF8f4HFJSbvv4xUj4yBCuPR4n6sI4Iz7UVsbw_ZVkzVbgYOXwC9uKufS8auJfV4ph-c0sQ1AICzlp7h4S8TsihoqAspH0vQ_Bp77Dn-XS7MFUZNvPge6ZSUqQCyI1L0hoPf6H3jJC6yV5nYnyfyg5WyIe-0wSbdO_WG13CoEF2hqmCN0f',
-
-    },
-  ];
-
-  final List<Map<String, String>> recommendations = const [
-    {
-      'title': 'New Sneakers',
-      'subtitle': 'Check out our latest basketball sneakers.',
-      'image':
-          'https://lh3.googleusercontent.com/aida-public/AB6AXuDiMr-33X4QDduGDebcdfwMOIDXgAI6qowG7XYx-840QI5DR1-NlVU5RrHBI7B9-d5ZGCq_5mkT76qC3_KYrLOikOzRww1cF8f4HFJSbvv4xUj4yBCuPR4n6sI4Iz7UVsbw_ZVkzVbgYOXwC9uKufS8auJfV4ph-c0sQ1AICzlp7h4S8TsihoqAspH0vQ_Bp77Dn-XS7MFUZNvPge6ZSUqQCyI1L0hoPf6H3jJC6yV5nYnyfyg5WyIe-0wSbdO_WG13CoEF2hqmCN0f',
-
-    },
-    {
-      'title': 'Running Shoes',
-      'subtitle': 'Top picks for your running sessions.',
-      'image':
-          'https://lh3.googleusercontent.com/aida-public/AB6AXuDiMr-33X4QDduGDebcdfwMOIDXgAI6qowG7XYx-840QI5DR1-NlVU5RrHBI7B9-d5ZGCq_5mkT76qC3_KYrLOikOzRww1cF8f4HFJSbvv4xUj4yBCuPR4n6sI4Iz7UVsbw_ZVkzVbgYOXwC9uKufS8auJfV4ph-c0sQ1AICzlp7h4S8TsihoqAspH0vQ_Bp77Dn-XS7MFUZNvPge6ZSUqQCyI1L0hoPf6H3jJC6yV5nYnyfyg5WyIe-0wSbdO_WG13CoEF2hqmCN0f',
-
-    },
-  ];
-
-  List<Map<String, String>> get currentNotifications {
-    switch (selectedTabIndex) {
-      case 0:
-        return promotions;
-      case 1:
-        return orderUpdates;
-      case 2:
-        return recommendations;
-      default:
-        return promotions;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    String selectedCategory = categories[selectedTabIndex];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFFF1F5F9),
@@ -102,74 +47,108 @@ class _NotificationScreenState extends State<NotificationScreen> {
             padding: const EdgeInsets.symmetric(vertical: 8),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _buildTab('Promotions', 0),
-                _buildTab('Order Updates', 1),
-                _buildTab('Recommendations', 2),
-              ],
+              children: List.generate(
+                categories.length,
+                (index) => _buildTab(
+                  categories[index].replaceAll('_', ' ').capitalize(),
+                  index,
+                ),
+              ),
             ),
           ),
           const Divider(height: 0, color: Color(0xFFCEDAE8)),
-          // Notification List
+
+          // Firestore Stream
           Expanded(
-            child: ListView.builder(
-              itemCount: currentNotifications.length,
-              itemBuilder: (context, index) {
-                final item = currentNotifications[index];
-                return ListTile(
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 8,
-                  ),
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      item['image']!,
-                      width: 56,
-                      height: 56,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  title: Text(
-                    item['title']!,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF0D141C),
-                    ),
-                  ),
-                  subtitle: Text(
-                    item['subtitle']!,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      color: Color(0xFF49709C),
-                    ),
-                  ),
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection("UserNotification")
+                  .where("category", isEqualTo: selectedCategory)
+                  .orderBy("createdAt", descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return const Center(child: Text("No notifications found"));
+                }
+
+                final docs = snapshot.data!.docs;
+
+                return ListView.builder(
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) {
+                    final data = docs[index].data() as Map<String, dynamic>;
+                    bool isRead = data['isRead'] ?? false;
+
+                    return ListTile(
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          data['image'] ?? '',
+                          width: 56,
+                          height: 56,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              const Icon(Icons.image_not_supported),
+                        ),
+                      ),
+                      title: Text(
+                        data['title'] ?? '',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: !isRead
+                              ? const Color.fromARGB(255, 250, 31, 15)
+                              : const Color(0xFF0D141C),
+                        ),
+                      ),
+                      subtitle: Text(
+                        data['subtitle'] ?? '',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: !isRead
+                              ? const Color.fromARGB(255, 0, 0, 0)
+                              : const Color(0xFF49709C),
+                        ),
+                      ),
+                      onTap: () async {
+                        if (!isRead) {
+                          await FirebaseFirestore.instance
+                              .collection("UserNotification")
+                              .doc(docs[index].id)
+                              .update({"isRead": true});
+                        }
+                      },
+                    );
+                  },
                 );
               },
             ),
           ),
         ],
       ),
-     
     );
   }
 
   Widget _buildTab(String title, int index) {
     bool isSelected = selectedTabIndex == index;
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedTabIndex = index;
-        });
-      },
+      onTap: () => setState(() => selectedTabIndex = index),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(
             title,
             style: TextStyle(
-              color: isSelected ? const Color(0xFF0D141C) : const Color(0xFF49709C),
+              color: isSelected
+                  ? const Color(0xFF0D141C)
+                  : const Color(0xFF49709C),
               fontWeight: FontWeight.bold,
             ),
           ),
@@ -186,4 +165,8 @@ class _NotificationScreenState extends State<NotificationScreen> {
       ),
     );
   }
+}
+
+extension StringCasingExtension on String {
+  String capitalize() => "${this[0].toUpperCase()}${substring(1)}";
 }
